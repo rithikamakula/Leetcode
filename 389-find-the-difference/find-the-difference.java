55f8d59eaca36013cb9919fc92deb593e96652e9
rithikamakula <rithikamakula@gmail.com>
2026-07-15 23:09:56 +0530

    Time: 3 ms (41.25%) | Memory: 43 MB (85.25%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,22 +1,17 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        HashMap<Character,Integer>hm1=new HashMap<>();
-        HashMap<Character,Integer>hm2=new HashMap<>();
+        int sum1=0;
         for(int i=0;i<s.length();i++){
             char ch1=s.charAt(i);
-            hm1.put(ch1,hm1.getOrDefault(ch1,0)+1);
-       }
-       for(int i=0;i<t.length();i++){
-            char ch2=t.charAt(i);
-            hm2.put(ch2,hm2.getOrDefault(ch2,0)+1);
-       }
-       char ans=' ';
-       for(int i=0;i<t.length();i++){
-        char ch2=t.charAt(i);
-        if(!hm1.containsKey(ch2)||!hm1.get(ch2).equals(hm2.get(ch2))){
-            ans=ch2;
+            sum1+=ch1;
         }
-       }
-       return ans;
+        int sum2=0;
+        for(int i=0;i<t.length();i++){
+            char ch2=t.charAt(i);
+            sum2+=ch2;
+        }
+        char ans=(char)(sum2-sum1);
+        return ans;
+
     }
 }
